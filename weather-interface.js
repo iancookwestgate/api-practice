79ab782b6e7e3a3335420af5ad1bc9dfd53ebae8
Ian Cook Westgate <iwestgate@hotmail.com>
2019-03-18 18:33:11 -0700

    change js to incorporate XML api calling.

--- weather-interface.js
+++ weather-interface.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    let city = $('#location').val();
+    const city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a45f23a4b138fa7a02a3d8e0fb7255d8`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[a45f23a4b138fa7a02a3d8e0fb7255d8]`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
       }
-    });
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
   });
 });
